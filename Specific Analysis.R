a452ae987111bbeff86ff68bb209f59490cf1c5a
TeresavL <t.linde@stud.uni-heidelberg.de>
2019-06-05 17:50:06 +0200

    Heatmap upordownregulation Biomarker
    Influence of cisplatin on the biomarkers gene expression in different cell lines

--- Specific Analysis.R
+++ Specific Analysis.R
@@ -15,7 +15,7 @@ while(j<820)
 
 FC.cisplatin = log2FC.treated.untreated[,cisplatin.col]
 mean.FC.rows <- apply(FC.cisplatin, 1, mean)
-sorted.mean = sort(mean.FC, decreasing = TRUE)
+sorted.mean = sort(mean.FC.rows, decreasing = TRUE)
 
 #finding/visualizing most extreme FC values for cisplatin
 lowest.FC <- sorted.mean[13290:13299]
@@ -27,12 +27,12 @@ par(mar = c(5, 5, 5, 5))
 barplot(highest.FC, horiz = TRUE, xlim = c(0,1.8), main= "highest log2 FC-values for cisplatin", xlab= "mean log2FC values in different celllines", col= "lightgreen",las=1, cex.names =0.8)
 
 #upordownregulation
-upordown = (treated.scaled/untreated.scaled)
-upordown = upordown[,c(95:149)]
-upordown = t(upordown)
-upordown.biomarker = upordown[ , c("SUPT3H","BBS9", "ANKRA2", "GNBL1", "KDM4C", "DDIT3", "TTC28","NBEA", "PCCA","PPP1R15A", "FTO", "PLK1", "VPS8", "POLR3B", "MAPKAP1", "STAG1", "TBCD", "C11orf49", "ANKS1A", "COMMD10")]
-upordown.biomarker = t(upordown.biomarker)
-heatmap(upordown.biomarker, Rowv=NA, Colv=NA, col = cm.colors(256), scale="column", margins=c(5,10))
+biomarker.1 <- c("SUPT3H", "BBS9", "ANKRA2","GNB1L", "KDM4C", "DDIT3", "TTC28","NBEA", "PCCA","PPP1R15A", "FTO", "PLK1", "VPS8","POLR3B", "MAPKAP1", "STAG1", "TBCD", "C11orf49", "ANKS1A", "COMMD10")
+biomarker = FC.cisplatin[c("SUPT3H", "BBS9", "ANKRA2","GNB1L", "KDM4C", "DDIT3", "TTC28","NBEA", "PCCA","PPP1R15A", "FTO", "PLK1", "VPS8","POLR3B", "MAPKAP1", "STAG1", "TBCD", "C11orf49", "ANKS1A", "COMMD10"),]
+colfunc <- colorRampPalette(c("firebrick", "firebrick3", "lightcoral","lightyellow","lightskyblue1","steelblue1", "steelblue3", "darkblue"))
+colfunc(25)
+heatmap.2(biomarker, dendrogram = "row", col = colfunc(25), scale="column", margins=c(4,8), xlab= "celllines", ylab ="biomarker for cisplatin", labCol = FALSE, trace = "none", key =TRUE, keysize = 0.25, density.info = "histogram", key.title = "red = downregulation, blue =upregulation", key.ylab = "count", lmat=rbind( c(0, 3), c(2,1), c(0,4) ), lhei = c(0.5,1,0.5))
+title("Influence of cisplatin on the biomarkers gene expression", line =-3)
 
 #biomarker Kriterium 2
 biomarker.kriterium2 = log2FC.treated.untreated
