0fb24baa6fedbb7413bda2e8a26fe0adef326da6
AmelieVogt <50015694+AmelieVogt@users.noreply.github.com>
2019-06-18 15:38:13 +0200

    delete old code

--- RGES.R
+++ RGES.R
@@ -12,36 +12,6 @@ treated = readRDS(paste0(wd, "/data/NCI_TPW_gep_treated.RDS"))
 basalexp = readRDS(paste0(wd, "/data/CCLE_basalexpression.RDS"))
 library(DESeq)
 
-#scale data
-treated <- scale(treated)
-untreated <- scale(untreated)
-#basal auch scale
-
-### drug_signature Doris Versuch
-mode(treated) <- "integer"
-cds.treated <- newCountDataSet(countData=treated, condition=c(rep("treated",819))) 
-cds.treated <- estimateSizeFactors(cds.treated)
-cds.treated <- estimateDispersions(cds.treated, method="per-condition")
-
-mode(untreated) <- "integer"
-cds.untreated <- newCountDataSet(countData = untreated, condition = c(rep("untreated",819)))
-cds.untreated <- estimateSizeFactors(cds.untreated)
-cds.untreated <- estimateDispersions(cds.untreated, method="per-condition")
-# glaub brauchen wir eigentlich nicht von hier:
-cols.treated <- conditions(cds.treated) == "A"
-cols.untreated <- conditions(cds.untreated) == "B"
-
-bmvA <- getBaseMeansAndVariances( counts(cds.treated)[,cols.treated], sizeFactors(cds.treated)[cols.treated] )
-bmvB <- getBaseMeansAndVariances( counts(cds.untreated)[,cols.untreated], sizeFactors(cds.untreated)[cols.untreated] )
-# bis hier.
-str( fitInfo( cds.treated ) )
-treated.dispersions <- fData(cds.treated)
-str( fitInfo( cds.untreated ) )
-untreated.dispersions <- fData(cds.untreated)
-
-p.values <- nbinomTestForMatrices(counts(cds.treated), counts(cds.untreated), sizeFactors(cds.treated), sizeFactors(cds.untreated), treated.dispersions[,1], untreated.dispersions[,1] )
-names(p.values) <- row.names(counts(cds.treated))
-
 # anderer Ansatz
 mode(treated) <- "integer"
 mode(untreated) <- "integer"
