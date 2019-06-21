6d7575a8ca83a95ea62e61536aa885dd9b5c0fb5
AmelieVogt <50015694+AmelieVogt@users.noreply.github.com>
2019-06-21 14:16:30 +0200

    add cmap_score_new function

--- RGES.R
+++ RGES.R
@@ -118,12 +118,66 @@ dim(drug_signature)
 disease_signature = subset(disease_signature, rownames(disease_signature) %in% gene.list.final)
 dim(disease_signature)
 
+## function from Bin Chen to compute RGES
+cmap_score_new <- function(sig_up, sig_down, drug_signature) {
+  num_genes <- nrow(drug_signature)
+  ks_up <- 0
+  ks_down <- 0
+  connectivity_score <- 0
+  drug_signature[,"rank"] <- rank(drug_signature[,"rank"])
+  up_tags_rank <- merge(drug_signature, sig_up, by.x = "ids", by.y = 1)
+  down_tags_rank <- merge(drug_signature, sig_down, by.x = "ids", by.y = 1)
+  up_tags_position <- sort(up_tags_rank$rank)
+  down_tags_position <- sort(down_tags_rank$rank)
+  num_tags_up <- length(up_tags_position)
+  num_tags_down <- length(down_tags_position)
+  if(num_tags_up > 1) {
+    a_up <- 0
+    b_up <- 0
+    a_up <- max(sapply(1:num_tags_up,function(j) {
+      j/num_tags_up - up_tags_position[j]/num_genes
+    }))
+    b_up <- max(sapply(1:num_tags_up,function(j) {
+      up_tags_position[j]/num_genes - (j-1)/num_tags_up
+    }))
+    if(a_up > b_up) {
+      ks_up <- a_up
+    } else {
+      ks_up <- -b_up
+    }
+  }else{
+    ks_up <- 0
+  }
+  if (num_tags_down > 1){
+    a_down <- 0
+    b_down <- 0
+    a_down <- max(sapply(1:num_tags_down,function(j) {
+      j/num_tags_down - down_tags_position[j]/num_genes
+    }))
+    b_down <- max(sapply(1:num_tags_down,function(j) {
+      down_tags_position[j]/num_genes - (j-1)/num_tags_down
+    }))
+    if(a_down > b_down) {
+      ks_down <- a_down
+    } else {
+      ks_down <- -b_down
+    }
+  }else{
+    ks_down <- 0
+  }
+  if (ks_up == 0 & ks_down != 0){ #only down gene inputed
+    connectivity_score <- -ks_down 
+  }else if (ks_up !=0 & ks_down == 0){ #only up gene inputed
+    connectivity_score <- ks_up
+  }else if (sum(sign(c(ks_down,ks_up))) == 0) {
+    connectivity_score <- ks_up - ks_down # different signs
+  }else{
+    connectivity_score <- ks_up - ks_down
+  }
+  return(connectivity_score)
+}
 
-###das muss man noch schaffen dass es corefunctions automatisch für alle reinläd und dann vllt auch aus den 
-#corefunctions die Funktionen löschen die wir nicht brauchen damit das environment nicht so sinnlos 
-#voll wird
-code_dir <- "C:/Users/amvog/Documents/" ##anpassen
-source(paste(code_dir, "core_functions.R",sep=""))
+
 
 ##CLAUDIA 
 sig.ids <- c(1:819) ### cell lines bekommen sig.ID: sig.ID=nrow(meta[,1:819])=ncol(treated usw)
@@ -143,5 +197,5 @@ for(count in sig.ids){
 }
 
 results <- data.frame(sig.ID = sig.ids, RGES = dz_cmap_scores)
-results <- merge(results, meta) 
+results <- cbind(results, meta[1:819,]) 
 
