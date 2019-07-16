8c5326f9907e820596f8eda8b8a98eebbffa8a55
johannabauer <johanna_bau@web.de>
2019-07-16 23:13:53 +0200

    markdowns broad, further; further corrected

--- 4.Further Analysis.R
+++ 4.Further Analysis.R
@@ -7,11 +7,23 @@ results = readRDS(paste0(wd, "/data/results.RDS"))
 results_cisplatin = subset (results , drug == "cisplatin")
 
 ###RGES analysis
-## overview RGES values - damit wir besser einschaetzen koennen was fuer uns gut oder schlecht ist
+## overview RGES values
 quantile(results$RGES)
 quantile(results_cisplatin$RGES)
 min = min(results$RGES)
 max = max(results$RGES)
+
+#plot
+quantile.res = as.matrix(quantile(results$RGES))
+plot(density(results$RGES))
+abline(v= quantile.res[c(2,4),1], col= c("red", "blue"), lty =2)
+legend(-6, 1.2, legend = c("25 % quantile", "75% quantile"), col = c("red", "blue"), lty = 1:2)
+
+quantile.cis = as.matrix(quantile(results_cisplatin$RGES))
+plot(density(results_cisplatin$RGES))
+abline(v= quantile.cis[c(2,4),1], col= c("red", "blue"), lty =2)
+legend(-6, 1.2, legend = c("25 % quantile", "75% quantile"), col = c("red", "blue"), lty = 1:2)
+
 
 # RGES cisplatin in general
 boxplot(results_cisplatin$RGES, main = "RGES for cisplatin in different celllines")
@@ -22,14 +34,12 @@ mean_tissue =sapply(1:length(tissue), function(x) mean(subset(results_cisplatin$
 barplot(mean_tissue , name = tissue , las = 2 , horiz = FALSE ,col= "firebrick", border = "white" , main = "mean RGES for cisplatin in different tissues" , ylab = "RGES")
 nrow(min(results_cisplatin$RGES))
 
-
 # mean RGES for different drugs
 drug = c("5-Azacytidine", "bortezomib", "cisplatin","dasatinib","doxorubicin","erlotinib","geldanamycin","gemcitibine","lapatinib","paclitaxel","sirolimus","sorafenib","sunitinib","topotecan","vorinostat")
 mean_drug =sapply(1:length(drug), function(x) mean(subset(results$RGES , drug == drug[x])))
 barplot(mean_drug , name = drug , las = 2 , horiz = FALSE ,col= "forestgreen", border = "white" , main = "mean RGES for different drugs" , ylab = "RGES")
 
 #distribution RGES in different drugs
-#macht ein neues Fenster auch (dev.new) weil ich finde man erkennt das in dem plot Fenster nicht
 dev.new(width=5, height=4)
 par(mfrow=c(5,3), mar=c(2,2,2,2))
 sapply(1:length(drug),
@@ -40,18 +50,9 @@ sapply(1:length(drug),
 # find negative RGES values
 results_neg =results[which(results$RGES < 0),]
 
-#sind diese drugs in allen celllines gut?
-boxplot(results[which(results$drug == "bortezomib"),2], main = "RGES for bortezomib in different celllines",  ylim = c(min,max))
-boxplot(results[which(results$drug == "paclitaxel"),2], main = "RGES for paclitaxel in different celllines",  ylim = c(min,max))
 
-#nein es sind f?r beide drugs nur Ausrei?er - hat es dann vllt etwas mit den celllines zu tun? 
-boxplot(results[which(results$cell == "UACC-62"),2], main = "RGES for cellline UACC-62 (Melanoma)",  ylim = c(min,max))
-boxplot(results[which(results$cell == "OVCAR-4"),2], main = "RGES for cellline OVCAR-4 (Ovarian)",  ylim = c(min,max))
 
-#auch hier sind es Ausrei?er. Das hei?t die guten RGES Werte liegen allein an der speziellen Kombination cellline+drug
-#ich hab mal kurz gegoogelt und paclitaxel wird bei ovarienkarzinom verwendet, das macht also voll Sinn
-#bortezomib wird nicht f?r melanome eingesetzt sonder f?r multiples myelom. da k?nnen wir dann sagen dass das ein Hinweis sein k?nnte
-#das Medikament auch f?r Melanome zu testen
+
 
 ### IC50 spalte an results anfügen -> drug_activity_rges
 ##loading data
